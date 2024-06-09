3e9f4d006dadcf56c7d18174bb4182c99daf9775
jacobperry <perryjacob@outlook.com>
2024-06-09 10:03:18 +0000

    Updated UI and added legendary testing

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,49 @@ function App() {
   const [item, setItem] = createSignal<collectable>()
   const [collectables, setCollectables] = createSignal<collectable[]>([])
   const [lastCollected, setLastCollected] = createSignal<collectable[]>([])
+  const [pity, setPity] = createSignal(0)
 
-  createEffect(async ()=> {
+  createEffect(async () => {
     getCollectables()
   })
 
-  const getCollectables = async function() {
+  const testSystem = function () {
+    if (item()?.rarity != "legendary") {
+      getRandomCollectable()
+      setTimeout(() => {
+        testSystem()
+      }, 50)
+    } else return
+
+  }
+
+  const getCollectables = async function () {
     const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/collectables/`)
     const data = await res.json();
     setCollectables(data)
   }
 
-  const getRandomCollectable = async function() {
-    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/collectables/random`)
+  const getRandomCollectable = async function () {
+
+    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/collectables/random`,
+      {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({"pity": pity()})
+      })
+
     const data: collectable = await res.json()
 
+    setPity(pity() + 1)
+    if (item()?.rarity == "legendary") setPity(1)
+
     let tempArr = lastCollected();
-    if(tempArr.length >= 10) {
+    if (tempArr.length >= 10) {
       tempArr = tempArr.slice(1)
     }
     tempArr.push(data)
-    console.log(tempArr)
     setLastCollected([...tempArr])
 
     setItem(data);
@@ -47,7 +69,7 @@ function App() {
             Try your luck!
           </button>
           <Show when={item()}>
-            <div class='card'>
+            <div class=''>
               <p class={`puff-name ${item()?.rarity.trimEnd().replace(" ", "-")}`}> {item()?.name} </p>
               <img src={item()?.img} alt="" />
             </div>
@@ -56,26 +78,33 @@ function App() {
         <aside>
           <h2>Current Collectables</h2>
           <Show when={collectables()} fallback={<div>Loading...</div>}>
-            <ul style={{"list-style": "none", 
+            <ul style={{
+              "list-style": "none",
               "text-align": 'left'
-              }}>
+            }}>
               {collectables().map(c => {
                 return (
                   <li>
                     {c.name} : <span class={c?.rarity.trimEnd().replace(" ", "-")}>{c.rarity}</span>
                   </li>
                 )
-                })}
+              })}
             </ul>
           </Show>
           <h3>Previous Pulls</h3>
+          <p class='pulls-since'>Pulls since last Legendary: <span>{pity()}</span></p>
           <ul class='previous-pulls'>
             {lastCollected().map(c => {
               return (<li class={c?.rarity.trimEnd().replace(" ", "-")}>{c.name}</li>)
-              })}
+            })}
           </ul>
         </aside>
       </div>
+      <Show when={import.meta.env.DEV}>
+        <button onClick={() => testSystem()}>
+          Break everything!
+        </button>
+      </Show>
     </>
   )
 }
